97e44943411b22667d564cc4f66eb87b583381f7
Grady <grady@grady-VirtualBox.(none)>
2013-04-26 15:08:32 -0700

    Added new C/Cython LLL files

--- lll.py
+++ lll.py
@@ -23,24 +23,24 @@ def gram_schmidt(g, m, mu, B):
 
   row = len(m)
 
-  for i in xrange(row):
+  for i in range(row):
     # bi* = bi
     b_i = g[:,i]
     b_i_star = b_i
     m[:, i] = b_i_star
 
-    for j in xrange(i):
+    for j in range(i):
       # u[i][j] = (bi, bj*)/Bj
       b_j_star = m[:, j]
       b_i = g[:, i]
-      B[j] = numpy.vdot(b_j_star, b_j_star)
-      mu[i,j] = numpy.vdot(b_i, b_j_star)[0,0]/ float(B[j,0])
+      B[j] = numpy.dot(b_j_star, b_j_star)
+      mu[i,j] = numpy.dot(b_i, b_j_star)/(B[j])
       b_i_star = b_i_star - b_j_star*mu[i,j]
       m[:, i] = b_i_star
 
     b_i_star = m[:, i]
     # B[i] = (bi*, bi*)
-    B[i] = numpy.vdot(b_i_star, b_i_star)
+    B[i] = numpy.dot(b_i_star, b_i_star)
  
 
 # Performs main computation of step 1 of the LLL algorithm (achieves 1.18)
@@ -57,29 +57,31 @@ def reduce(g, mu, k, l):
 
     mu[k,l] = mu[k,l] - r
 
-
 # Main LLL Reduction
 def lll_reduction(n, lc=0.75):
 
   dim = n.shape[0]
 
-  m = numpy.matrix(numpy.zeros(shape=(dim,dim)),int).astype(object)
-  mu = numpy.matrix(numpy.zeros(shape=(dim,dim)))
+  m = numpy.zeros((dim,dim), dtype = numpy.double)
+  mu = numpy.zeros((dim,dim), dtype = numpy.double)
   b = n
-  B = numpy.matrix(numpy.zeros(shape=(dim,1)),int).astype(object)
-
+  B = numpy.zeros(dim, dtype = numpy.double)
   gram_schmidt(b, m, mu, B)
-
+  print mu
+  print 
+  print B
   #k starts at 1 instead of 2 because of zero indexing
   k = 1
+  count = 0
 
   while 1:
+    print count
+    count = count + 1
     # Performs step 1 of the LLL algorithm
     reduce(b, mu, k, k-1)
-
     # Performs step 2 case 1, where case 1 
     #=> condition 1.20 from LLL paper
-    if (B[k,0] < lc*B[k-1,0] - mu[k,k-1]**2 * B[k-1,0] and k > 0):
+    if (B[k] < lc*B[k-1] - mu[k,k-1]**2 * B[k-1] and k > 0):
 
       #First exchange b_k and b_(k-1)
       tmp = numpy.matrix(b[:, k])
@@ -91,19 +93,17 @@ def lll_reduction(n, lc=0.75):
       u = mu[k,k-1]
       
       #Next adjust the set B (referred to as C* in LLL)
-      B_temp = B[k,0]
-      C = B[k,0] + mu[k,k-1]**2 * B[k-1,0]
-      mu[k,k-1] = mu[k,k-1]*B[k-1,0]/(1.0*C)
-      B[k,0] = B[k-1,0] * B[k,0]/C
-      B[k-1,0] = C
+      B_temp = B[k]
+      C = B[k] + B[k-1] * mu[k,k-1]**2
+      mu[k,k-1] = mu[k,k-1]*B[k-1]/(1.0*C)
+      B[k] = B[k-1] * B[k]/C
+      B[k-1] = C
       #All other B[i] remains the same
-
       #Adjust the set mu next
       #for  i > k
       for i in xrange(k+1, dim):
         temp = mu[i,k-1]
-        print C[k-1,0]
-        mu[i,k-1] = mu[i,k-1]*mu[k,k-1] + mu[i,k]*B_temp/(1.0*B[k-1,0])
+        mu[i,k-1] = mu[i,k-1]*mu[k,k-1] + mu[i,k]*B_temp/(1.0*B[k-1])
         #recall that u is the original mu[i,k]
         mu[i,k] = temp - mu[i,k]*u
 
@@ -115,19 +115,13 @@ def lll_reduction(n, lc=0.75):
       
       #All other values of mu remain the same
       k = k - 1
-      print k
-
     else:
       l = k-1
       while l >= 0:
         if math.fabs(mu[k,l]) > 0.5: 
           reduce(b, mu, k, l)
-          l = k-1
-        else:
-          l = l - 1
-      k = k + 1
-      
-    print k
+        l = l - 1
+      k = k + 1   
     if (k == dim):
       return b
 
@@ -136,20 +130,22 @@ def islll(n, lc=0.75):
 
   dim = n.shape[0]
 
-  m = numpy.matrix(numpy.zeros(shape=(dim,dim)),int).astype(object)
-  mu = numpy.matrix(numpy.zeros(shape=(dim,dim)))
-  B = numpy.matrix(numpy.zeros(shape=(dim,1)),int).astype(object)
+  m = numpy.zeros((dim,dim))
+  mu = numpy.zeros((dim,dim))
+  B = numpy.zeros((dim,1))
 
   gram_schmidt(n, m, mu, B)
 
   for i in xrange(dim):
     for j in xrange(i):
-      if math.fabs(mu[i,j]) > 0.5:
+      if math.fabs(mu[i,j]) > 0.50:
         print "Hello"
+        print i,j
         return False
 
   for k in xrange(1, dim):  
     if B[k] < (lc*B[k-1,0] - B[k-1,0]*mu[k,k-1]*mu[k,k-1]):
+      print "HI"
       return False
 
   return True
